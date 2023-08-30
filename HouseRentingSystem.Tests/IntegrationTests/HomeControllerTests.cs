ccc031f196e1aef5e76a46571ce8303cc1ccc536
evandonova <evelina.and15@gmail.com>
2023-08-30 11:15:14 +0300

    Added integration tests

---
+++ HouseRentingSystem.Tests/IntegrationTests/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HouseRentingSystem.Web.Controllers;
+
+namespace HouseRentingSystem.Tests.IntegrationTests
+{
+    public class HomeControllerTests
+    {
+        private HomeController homeController;
+
+        [OneTimeSetUp]
+        public void SetUp() 
+            => this.homeController = new HomeController(null!);
+
+        [Test]
+        public void Error_ShouldReturnCorrectView()
+        {
+            // Arrange: assign a valid status code to a variable
+            var statusCode = 500;
+
+            // Act: invoke the controller method with valid data
+            var result = this.homeController.Error(statusCode);
+
+            // Assert the returned result is not null
+            Assert.IsNotNull(result);
+
+            // Assert the returned result is a view
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
